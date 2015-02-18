d71341bb5a21bfa91ff88b9bef7b57d3b10b344b
Craigson <craigpick@gmail.com>
2015-02-18 10:40:06 -0500

    api call works with single input

--- speech_test_copy.js
+++ speech_test_copy.js
@@ -27,12 +27,6 @@ var params = {
   outputMode: 'json'
 }
 
-recognition.onend = function(event){
-  console.log(list);
-  //save all of this into an array
-  go();
-};
-
 recognition.onspeechstart = function(event){
 console.log("speech has started");
 go();
@@ -46,65 +40,53 @@ go();
 function go(){
 
   console.log("go() being called");
+  var i = 0;
+
+
   recognition.onresult = function(event){
 
-    var text = '';
-    console.log("starting recording")
-    // Assemble the transcript from the array of results
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-            text += event.results[i][0].transcript;
-            // corpus += text;
-        }
+    var res = event.results[i][0].transcript;
+    var sentiment = 0;
+    
+    var params = {
+      text: res,
+      apikey: 'YOUR_KEY',
+      outputMode: 'json'
     }
 
-    console.log("adding to array");
-    list.push(text);
-    if (list.length > 4){
-    console.log("it's full");
-      getSentiment(list);
+    var url = 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment';
+
+    console.log("making api call");
+    $.getJSON(url, params, function(data) {
+      // $('body').append(res+' ('+data.docSentiment.type+':'+data.docSentiment.score+')<br>');
+      sentiment = data.docSentiment.score;
+      console.log("text: " + res);
+      console.log("sentiment: "+ sentiment);
+    });
+
+    if (res.indexOf('color') !== -1 && sentiment < 0){
+        changePalette = true;
+        console.log("changing color of artwork");
+      } else if (res.indexOf('composition') !== -1 && sentiment < 0){
+        changeComp = true;
+        console.log("changing composition of artwork");
     }
+
+    i++;
   };
 }
 
-//not sure on using list parameter here
-function getSentiment(list){
-
-console.log("making api call");
-/*
-  var url = 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment';
-  var obj = {};
-
-  for (var i = 0; i < list.length; i++){
-    params.text = list[i];
-
-    $.getJSON(url, params, function(data) {
-      console.log(typeof data);
-      obj = data;
-      console.log(typeof obj);
-      console.log(data);
-      // $('body').append(res+' ('+data.docSentiment.type+':'+data.docSentiment.score+')<br>');
-      var sentiment = data.docSentiment.score;
-    });
-
  
     
-    if (res.indexOf('color') !== -1 && sentiment < 0){
-        changePalette = true;
-      } else if (res.indexOf('composition') !== -1 && sentiment < 0){
-        changeComp = true;
-      }
-  }
+/*
 
   console.log("was: " + changePalette + " " + changeComp + " " + list.length)
   changeVariables();
   changePalette = false;
   changeComp = false;
-  list.length = 0;   
   console.log("is now: " + changePalette + " " + changeComp + " " + list.length)
+*/
 
-  */
-}
 
 function changeVariables(){
 //code in here to change p5 sketch
